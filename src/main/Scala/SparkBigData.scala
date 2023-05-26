9f6645d2c318066dd08b9bb321c77a41757ad915
Ouedraogo <technidata225@gmail.com>
2023-05-26 02:07:22 +0000

    Manipulation des Rdd

--- src/main/Scala/SparkBigData.scala
+++ src/main/Scala/SparkBigData.scala
@@ -7,10 +7,40 @@ object SparkBigData {
 
   def main(args: Array[String]): Unit = {
     val sc = Session_spark(true).sparkContext
-    val rdd_test : RDD[String] = sc.parallelize(List("Ouedraogo","Djamila","Yennega"))
+    sc.setLogLevel("OFF")
+    /** Creation d'un rdd à partir d'une List
+    val rdd_test : RDD[String] = sc.parallelize(List("Ouedraogo","Djamila","Yennega","Djibril","Mamadi","Kassoum","Konate"))
     rdd_test.foreach{
       l => println(l)
     }
+    //Creation d'un rdd à partir d'un tableau
+
+    val rdd_test1 : RDD[String] = sc.parallelize(Array("Kone","Moussa","Ladji"))
+    rdd_test1.foreach{
+      l => println(l)
+    }
+    //Creation d'un rdd à partir d'une seq(sequence)
+
+    val mon_rdd  = sc.parallelize(Seq(("Ouedraogo" , "Math" , 15),("Diomandé" , "Math", 14),("Sylla","Math", 18),("Manassé" ,"Math",19)))
+    println("Mon premier element :")
+      mon_rdd.take(1).foreach(l=> println(l))
+
+   // println("Tous les elements :")
+   // mon_rdd.take(4).foreach(l=> println(l))
+
+    // Enregistrer sur disk
+    mon_rdd.repartition(1).saveAsObjectFile("C:\\Users\\PC\\Desktop\\mon test.txt") */
+
+    /** Creation d'un rdd à partir d'une source de données
+    val rdd2 = sc.textFile("C:\\Users\\PC\\Desktop\\Maîtrisez Spark pour le Big Data avec Scala\\sources\\test_rdd.txt")
+    println("Lecture du contenu de fichier text :")
+    rdd2.foreach(l=> println(l))
+    */
+
+// Le lineage ou  (plan d'execution)
+
+
+
 
   }
   /**
