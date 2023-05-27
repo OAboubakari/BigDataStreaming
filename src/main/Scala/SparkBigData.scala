958df449baed20831c89b4a526e9ab547a9c70a1
Ouedraogo <technidata225@gmail.com>
2023-05-27 18:11:51 +0000

    Operation sur les dataframes

--- src/main/Scala/SparkBigData.scala
+++ src/main/Scala/SparkBigData.scala
@@ -1,13 +1,47 @@
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql._
+import org.apache.spark.sql.functions._
+
+
 
 
 object SparkBigData {
   var ss : SparkSession = null
 
   def main(args: Array[String]): Unit = {
+    val session_s = Session_spark(true)
+
+    val df_test = session_s.read
+        .format("com.databricks.spark.csv")
+        .option("delimiter" , ",")
+        .option("header" ,"true")
+        .csv("C:\\Users\\PC\\Desktop\\Maîtrisez Spark pour le Big Data avec Scala\\sources de données\\csvs\\2010-12-06.csv")
+
+    df_test.show(5)
+    val df_2 = df_test.select(col("InvoiceNo") , col("StockCode"), col("Quantity"), col("_c0").alias("Id du Client"), col("Description"))
+    df_2.show(5)
+
+    /* Charger plusieurs dataframes
+    val df_group = session_s.read
+      .format("csv")
+      .option("inferSchema" , ",")
+      .option("header" ,"true")
+      .load("C:\\Users\\PC\\Desktop\\Maîtrisez Spark pour le Big Data avec Scala\\sources de données\\csvs\\")
+    */
+
+    //df_test.show(5)
+     //df_test.printSchema()
+
+
+   // println("df_test_count : "+df_test.count() +"df_group :"+df_group.count() )
+
+  }
+
+  def manipulation_rdd (): Unit = {
     val sc = Session_spark(true).sparkContext
+    val session_s = Session_spark(true)
     sc.setLogLevel("OFF")
+
     /** Creation d'un rdd à partir d'une List
     val rdd_test : RDD[String] = sc.parallelize(List("Ouedraogo","Djamila","Yennega","Djibril","Mamadi","Kassoum","Konate"))
     rdd_test.foreach{
@@ -32,13 +66,13 @@ object SparkBigData {
     mon_rdd.repartition(1).saveAsObjectFile("C:\\Users\\PC\\Desktop\\mon test.txt") */
 
     //d'un rdd à partir d'une source de données
-    val rdd2 = sc.textFile("C:\\Users\\PC\\Desktop\\Maîtrisez Spark pour le Big Data avec Scala\\sources\\test_rdd.txt")
+    //val rdd2 = sc.textFile("C:\\Users\\PC\\Desktop\\Maîtrisez Spark pour le Big Data avec Scala\\sources\\test_rdd.txt")
     //println("Lecture du contenu de fichier text :")
     //rdd2.foreach(l=> println(l))
 
     // Transformation des RDD
 
-    val rdd_trans : RDD[String] = sc.parallelize(List("alain mange une banane","la banane est un bon aliment pour la santé","achetez une bonne banane"))
+    //val rdd_trans : RDD[String] = sc.parallelize(List("alain mange une banane","la banane est un bon aliment pour la santé","achetez une bonne banane"))
     //rdd_trans.foreach(l=>println("Ligne de mon rdd : " +l))
     // Utilisation de la fction map
     //val rdd_map = rdd_trans.map(a => a.split(" "))
@@ -46,22 +80,32 @@ object SparkBigData {
 
     // Nombre de caractere
 
-    val rdd4 = rdd_trans.map( x => (x, x.length , x.contains("banane")))
+   // val rdd4 = rdd_trans.map( x => (x, x.length , x.contains("banane")))
 
 
-    val rdd5 = rdd4.map(x=> (x._1.toUpperCase() , x._3 , x._2))
+    //val rdd5 = rdd4.map(x=> (x._1.toUpperCase() , x._3 , x._2))
 
-    val rdd6 = rdd5.map(x=>(x._1.split(" ") ,1))
+   // val rdd6 = rdd5.map(x=>(x._1.split(" ") ,1))
 
     //rdd6.foreach(o => println(o._1(0),o._2))
     // Use of flatmap
     //val rdd_flat = rdd_trans.flatMap(x=>(x.split(" "))).map(x => (x,1))
-    val rdd3 = rdd2.flatMap(x=> x.split(",")).map(m =>(m,1))
+   // val rdd3 = rdd2.flatMap(x=> x.split(",")).map(m =>(m,1))
     //val rdd_plat = rdd3.flatMap(x=> x.split( " "))
-    rdd3.repartition(1).saveAsTextFile("C:\\Users\\PC\\Desktop\\essai_flat_vers2.txt")
+    //rdd3.repartition(1).saveAsTextFile("C:\\Users\\PC\\Desktop\\essai_flat_vers2.txt")
       //rdd_plat.foreach(x => println(x))
 
+   //val rdd_reduce = rdd_trans.flatMap(x => x.split( "  ")).map(w => (w ,1))
 
+   // val rdd_filtered = rdd_reduce.reduceByKey((x ,y ) => x + y)
+
+    //rdd_filtered.foreach(l =>println(l))
+
+//  Manipulation des dataframes
+
+    import session_s.implicits._
+    //val df : DataFrame = rdd_filtered.toDF("Texte" ,"Valeur")
+    //df.show(2)
 
 
 
