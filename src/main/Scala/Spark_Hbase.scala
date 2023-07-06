32817ef2e9db5eaa9aa466ad24f2f49ed24d5247
Ouedraogo <technidata225@gmail.com>
2023-07-06 00:40:15 +0000

    Definition du catalogue pour le mapping entre les Dataframes et  tables Hbases

--- src/main/Scala/Spark_Hbase.scala
+++ src/main/Scala/Spark_Hbase.scala
@@ -9,6 +9,26 @@ import SparkBigData._
 
 object Spark_Hbase {
 
+  // Definition du catalogue pour le mapping entre les Dataframes et  tables Hbases
+
+  def catalogue_hbase(): String = """{
+                                    |        |"table":{"namespace":"default", "name":"table_orders"},
+                                    |        |"rowkey":"key",
+                                    |        |"columns":{
+                                    |          |"orderid":{"cf":"rowkey", "col":"key", "type":"string"},
+                                    |          |"customer_id":{"cf":"orders", "col":"customerid", "type":"string"},
+                                    |          |"campaign_id":{"cf":"orders", "col":"campaignid", "type":"string"},
+                                    |          |"orderdate":{"cf":"orders", "col":"orderdate", "type":"string"},
+                                    |          |"city":{"cf":"orders", "col":"city", "type":"string"},
+                                    |          |"state":{"cf":"orders", "col":"state", "type":"string"},
+                                    |          |"zipcode":{"cf":"orders", "col":"zipcode", "type":"string"},
+                                    |          |"paymenttype":{"cf":"orders", "col":"paymenttype", "type":"string"},
+                                    |          |"totalprice":{"cf":"orders", "col":"totalprice", "type":"string"}
+                                    |		  |"numorderlines":{"cf":"orders", "col":"numorderlines", "type":"string"}
+                                    |        |}
+                                    |      |}
+                                    |	  """.stripMargin
+
 
   def main(args: Array[String]): Unit = {
 
