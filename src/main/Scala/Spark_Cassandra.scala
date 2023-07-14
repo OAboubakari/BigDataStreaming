8d29690944e0bec4ec70243e12093e1d158f2f6f
Ouedraogo <technidata225@gmail.com>
2023-07-14 01:23:07 +0000

    creation du catalogue cassandra

--- src/main/Scala/Spark_Cassandra.scala
+++ src/main/Scala/Spark_Cassandra.scala
@@ -2,8 +2,10 @@ import org.apache.spark.sql._
 import org.apache.spark.sql.types
 import org.apache.spark.sql.functions._
 import SparkBigData._
-
-
+import org.apache.spark.sql.cassandra._
+import com.datastax.spark.connector._
+import com.datastax.spark.connector.cql.CassandraConnector
+import org.apache.spark.sql.cassandra._
 
 object Spark_Cassandra {
 
@@ -12,6 +14,8 @@ object Spark_Cassandra {
     ss = Session_spark(true)
 // creation du catalog cassandra
 
+    ss.conf.set(s"spark.sql.catalog.sp_cassandra","com.datastax.spark.connector.datasource.CassandraCatalog")
+    ss.conf.set(s"spark.sql.catalog.sp_cassandra.spark.cassandra.connection.host", "127.0.0.1")
 
 
 
