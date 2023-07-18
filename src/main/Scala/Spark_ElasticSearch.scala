84704010f2f57f605c0ee91f9fac98a93f17c14f
Ouedraogo <technidata225@gmail.com>
2023-07-18 21:58:54 +0000

    Chargement et lecture du fichier à indexer avec Spark

--- src/main/Scala/Spark_ElasticSearch.scala
+++ src/main/Scala/Spark_ElasticSearch.scala
@@ -2,8 +2,24 @@ import org.apache.spark.sql._
 import org.apache.spark.sql.types
 import org.apache.spark.sql.functions._
 import SparkBigData._
+import org.elasticsearch.spark.sql._
+
 
 
 object Spark_ElasticSearch {
+
+  def main(args: Array[String]): Unit = {
+    // Lecture du fichier csv à indexer
+    val session_elasticsearch = Session_spark(true)
+    val df_index = session_elasticsearch.read
+      .format("com.databricks.spark.csv")
+      .option("delimiter" , ";")
+      .option("header" ,"true")
+      .load("C:\\Users\\PC\\Desktop\\Maîtrisez Spark pour le Big Data avec Scala\\sources de données\\orders.csv")
+
+
+    df_index.show(15)
+
+  }
 
 }
